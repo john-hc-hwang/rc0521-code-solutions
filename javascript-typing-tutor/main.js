d737250d5991f37f4b1c717c8136a05b4119603e
John Hwang <johnhchwang@gmail.com>
2021-05-21 17:50:27 -0700

    new features added

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,11 +1,20 @@
 var $body = document.querySelector('body');
-var $span = document.querySelectorAll('span');
+var $form = document.querySelector('.formtype');
 var $results = document.querySelector('.results.none');
 var $again = document.querySelector('.again.none');
+var $input = document.querySelector('#typehere');
+var $goButton = document.querySelector('.goButton');
+var $h1 = document.querySelector('h1');
 
 var count = 0;
 var errorCount = 0;
+
 function keydown(event) {
+  if (event.target.tagName === 'INPUT') {
+    return;
+  }
+
+  var $span = document.querySelectorAll('span');
   if (count < $span.length) {
     if ($span[count].innerText === event.key) {
       $span[count].className = 'correct';
@@ -26,14 +35,34 @@ function keydown(event) {
 }
 
 function reset(event) {
-  count = 0;
-  errorCount = 0;
+  var $span = document.querySelectorAll('span');
   for (var i = 0; i < $span.length; i++) {
     $span[i].removeAttribute('class');
+    $span[i].remove();
   }
+  $input.value = '';
+  count = 0;
+  errorCount = 0;
   $results.className = 'results none';
   $again.className = 'again none';
+  $form.className = 'formtype';
 }
 
+function gogo(event) {
+  var userInput = $input.value;
+  var $spanChar = document.createElement('span');
+  $spanChar.className = 'underline';
+  $spanChar.textContent = userInput[0];
+  $h1.appendChild($spanChar);
+  for (var i = 1; i < userInput.length; i++) {
+    $spanChar = document.createElement('span');
+    $spanChar.textContent = userInput[i];
+    $h1.appendChild($spanChar);
+  }
+
+  $form.className = 'formtype none';
+}
+
+$goButton.addEventListener('click', gogo);
 $body.addEventListener('keydown', keydown);
 $again.addEventListener('click', reset);
