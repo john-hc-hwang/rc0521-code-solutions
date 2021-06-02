5c77765ab219e824418e7db9e3483815c9beaaa6
John Hwang <johnhchwang@gmail.com>
2021-06-01 17:49:50 -0700

    Completed javascript-bank

---
+++ javascript-bank/account.js
@@ -0,0 +1,42 @@
+/* exported Account */
+
+function Account(number, holder, transactions) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  if (amount > 0 && Number.isInteger(amount)) {
+    var trans = new Transaction('deposit', amount);
+    this.transactions.push(trans);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (amount > 0 && Number.isInteger(amount)) {
+    var trans = new Transaction('withdrawal', amount);
+    this.transactions.push(trans);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  var balance = 0;
+  if (this.transactions.length === 0) {
+    return 0;
+  }
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      balance += this.transactions[i].amount;
+    } else if (this.transactions[i].type === 'withdrawal') {
+      balance -= this.transactions[i].amount;
+    }
+  }
+  return balance;
+};
