853370c6d388d87ec349afa8fc8a620cd5178b4c
John Hwang <johnhchwang@gmail.com>
2021-06-22 14:38:25 -0700

    Completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('typeof exports:', typeof exports); // object
+console.log('value of exports:', exports);
+
+console.log('typeof require:', typeof require); // function
+console.log('value of require:', require);
+
+console.log('typeof module:', typeof module); // object
+console.log('value of module:', module);
+
+console.log('typeof __filename:', typeof __filename); // string
+console.log('value of __filename:', __filename);
+
+console.log('typeof __dirname:', typeof __dirname); // string
+console.log('value of __dirname:', __dirname);
