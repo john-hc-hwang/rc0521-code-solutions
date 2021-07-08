f46957165138d2caf4706efdc91148f23d78e4cc
John Hwang <johnhchwang@gmail.com>
2021-07-08 11:23:46 -0700

    function component

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class CustomButton extends React.Component {
-  render() {
-    return <button>Click Me!</button>;
-  }
-}
+const CustomButton = () => {
+  return <button>Click Me!</button>;
+};
 
-const element = <CustomButton></CustomButton>;
 ReactDOM.render(
-  element,
+  <CustomButton />,
   document.getElementById('root')
 );
