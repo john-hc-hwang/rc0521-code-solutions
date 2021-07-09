b95d923cc8ed59c1a69a0f6270a455b525679979
John Hwang <johnhchwang@gmail.com>
2021-07-09 10:48:22 -0700

    Completed react-hot-button-component

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HotButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clickCount: 0 };
+
+    // This binding is necessary to make `this` work in the callback
+    this.countClick = this.countClick.bind(this);
+  }
+
+  countClick() {
+    this.setState({ clickCount: this.state.clickCount + 1 });
+  }
+
+  render() {
+    let className = 'white';
+    const state = this.state.clickCount;
+
+    if (state >= 3 && state < 6) {
+      className = 'purple';
+    } else if (state >= 6 && state < 9) {
+      className = 'lightPurple';
+    } else if (state >= 9 && state < 12) {
+      className = 'pink';
+    } else if (state >= 12 && state < 15) {
+      className = 'lightOrange';
+    } else if (state >= 15 && state < 18) {
+      className = 'yellow';
+    } else {
+      className = 'white';
+    }
+
+    return <button className={className} onClick={this.countClick}>Hot Button</button>;
+  }
+}
+
+ReactDOM.render(
+  <HotButton />,
+  document.querySelector('#root')
+);
